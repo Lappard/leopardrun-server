cdc6dbc5d6d0798837531064705d07a6a09762fb
Jonathan <Jonathan@nosfair.com>
2015-04-30 14:37:15 +0200

    no error on client close

--- Server/server.js
+++ Server/server.js
@@ -2,21 +2,43 @@
 var WebSocketServer = require('ws').Server, 	
 	wss = new WebSocketServer({ port: 1337 });
 
+if(wss){
+	console.log('Server started on port 1337');
+}
 
-var connections = [];
+var connections = [],
+	clientIndex = 0;
+
 
 wss.on('connection', function connection(ws) {
-
+	ws.clientIndex = clientIndex++;
   	ws.on('message', function incoming(message) {
   		connections.forEach(function(conn){
   			if(ws !== conn){
-  				conn.send('vom Server: '+message);
+				sendMessage(conn,'von clientNr: ' + ws.clientIndex + ': ' +message);
   			}
-  		})
+  		});
   	});
 
-  ws.send('Connected to the server!');
-
-  connections.push(ws)
+	sendMessage(ws, 'Connected to the server! ClientIndex: ' +(clientIndex-1));
+  	connections.push(ws);
 
 });
+
+
+function sendMessage(connection, msg) {
+	try { 
+		connection.send(msg); 
+	} catch (e) {
+		console.log(e);
+		 connections.forEach(function(conn){
+  			if(connection === conn){
+				console.log('deleted: ' + conn.clientIndex);
+  				var index = connections.indexOf(conn);
+  				if (index > -1) {
+					connections.splice(index, 1);
+				}
+  			}
+  		});
+	}
+}
