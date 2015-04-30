0cf7127eb7b119525a7c4ed490d6fdbe24c42582
Jonathan <Jonathan@nosfair.com>
2015-04-30 12:57:57 +0200

    init server

---
+++ Server/server.js
@@ -0,0 +1,52 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+ 
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+var connections = [];
+
+server.listen(1337, function() {
+    console.log((new Date()) + ' Server is listening on port 1337');
+});
+ 
+wsServer = new WebSocketServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+ 
+function originIsAllowed(origin) {
+	//accept logic
+  	return true;
+}
+ 
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin 
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+
+    connections.push(connection);
+
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
