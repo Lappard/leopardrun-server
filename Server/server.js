a3d011a37e7323cec113b89ef8d878bcda8479cf
Jonathan <Jonathan@nosfair.com>
2015-04-30 13:34:09 +0200

    send messages to all clients that are not sender

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,22 @@
+
 var WebSocketServer = require('ws').Server, 	
 	wss = new WebSocketServer({ port: 1337 });
 
-wss.on('connection', function connection(ws) {
-	
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
 
-  ws.send('something');
+var connections = [];
+
+wss.on('connection', function connection(ws) {
+
+  	ws.on('message', function incoming(message) {
+  		connections.forEach(function(conn){
+  			if(ws !== conn){
+  				conn.send('vom Server: '+message);
+  			}
+  		})
+  	});
+
+  ws.send('Connected to the server!');
+
+  connections.push(ws)
+
 });
