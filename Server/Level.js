16ea6ca951103c5aa43d40569ee62f598d3ef521
Jonathan <Jonathan@nosfair.com>
2015-06-04 11:02:22 +0200

    generate rendom height

--- Server/Level.js
+++ Server/Level.js
@@ -7,7 +7,7 @@ var Chance = require('chance');
  *
  * @constructor
  */
-function Level(){
+function Level() {
     var _level = [],
         _chance = new Chance();
 
@@ -15,42 +15,26 @@ function Level(){
         var result = {
             levelparts: []
         };
-        for (var i = 0; i < 10; ++i){
+        for (var i = 0; i < 10; ++i) {
             result.levelparts.push(_createLevelPart())
         }
         return result;
     };
-    
+
     var _createLevelPart = function () {
-        var result = [
-            //{"type":"g","x":0,"y":0},
-            //{"type":"g","x":1,"y":0},
-            //{"type":"g","x":2,"y":1},
-            //{"type":"g","x":3,"y":0},
-            //{"type":"g","x":4,"y":2},
-            //{"type":"g","x":5,"y":0},
-            //{"type":"b","x":0,"y":1},
-            //{"type":"b","x":2,"y":2},
-            //{"type":"b","x":3,"y":1},
-            //{"type":"b","x":5,"y":3}
-        ];
+        var result = [];
         // generate ground
-        for (var i = 0; i < 6; ++i){
-            result.push({"type":"g","x":i,"y":_chance.integer({min:0,max:1})})
-        }
-
-        for (var i = 0; i < 6; ++i){
-            if(_chance.bool({likelihood: 30})){
-                result.push({"type":"b","x":i,"y":_chance.integer({min:0,max:3})});
+        for (var i = 0; i < 6; ++i) {
+            var groundHeight = _chance.integer({min: 0, max: 1});
+            result.push({"type": "g", "x": i, "y": groundHeight})
+            if (_chance.bool({likelihood: 30})) {
+                result.push({"type": "b", "x": i, "y": _chance.integer({min: groundHeight + 1, max: 3})});
             }
-
         }
         _level.push(result);
-
         return result;
     };
 }
-
 
 
 module.exports = new Level();
